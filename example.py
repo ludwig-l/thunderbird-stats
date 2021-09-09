94e2538fdadfe09ba72111d2bd1f1dc376574ecc
Ludwig Lohmer <ludwig-lohmer@web.de>
2021-09-09 16:55:10 +0300

    Edit comment

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 """
-Simple example how 
+Simple example for demonstrating functionalities
 """
 
 from thunderbirdstats import ThunderbirdStats as ts
 
 # definitions
-path = "/home/ludwig/.thunderbird/bdt6qcqz.default/ImapMail/mail.stusta.de/Sent-1"
+path = "/home/ludwig/.thunderbird/bdt6qcqz.default/ImapMail/mail.stusta.de/Sent-2"
 obj = ts(path)
 
 # call functions
